b45aae44cb71d9268f31d491d962142fbdda91ee
Md Tanzim Hossain Mridha <tanzimjafirwadi@gmail.com>
2025-06-24 20:00:47 +0600

    Update script.js

--- script.js
+++ script.js
@@ -16,12 +16,31 @@ const Player2 = [
     document.querySelector('.P2img6'),
 ]
 
+window.onload = () => {
+  // Set initial message
+  document.querySelector("h1").innerHTML = "Press the Play Button To Start";
+
+  // Show only the first image of Player 1
+  const player1Images = document.querySelectorAll('.dice img');
+  player1Images.forEach((img, index) => {
+    img.style.display = index === 0 ? 'block' : 'none';
+  });
+
+  // Show only the first image of Player 2
+  const player2Images = document.querySelectorAll('.dice-2 img');
+  player2Images.forEach((img, index) => {
+    img.style.display = index === 0 ? 'block' : 'none';
+  });
+};
+
 function DiceChallenge() {
+
     const RandomNumberP1 = Math.floor(Math.random() * Player1.length);
     const SelectedImageP1 = Player1[RandomNumberP1];
 
     const RandomNumberP2 = Math.floor(Math.random() * Player2.length);
     const SelectedImageP2 = Player2[RandomNumberP2]
+
 
     // Hide all the Images of SelectedImageP1 except first Images
     Player1.forEach((img, index) => {
@@ -42,4 +61,6 @@ function DiceChallenge() {
     }
 }
 
-window.onload = DiceChallenge();
+DiceChallenge();
+
+document.querySelector("button").onclick = DiceChallenge;
